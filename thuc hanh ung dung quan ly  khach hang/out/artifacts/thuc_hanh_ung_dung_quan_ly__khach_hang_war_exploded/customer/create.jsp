b5ab1c9e89dc978ef98568f0a445bfdd7c0cc235
Dinh Phu <ndp17hp@gmail.com>
2020-06-17 16:25:05 +0700

    them file bai tap

--- thuc hanh ung dung quan ly  khach hang/out/artifacts/thuc_hanh_ung_dung_quan_ly__khach_hang_war_exploded/customer/create.jsp
+++ thuc hanh ung dung quan ly  khach hang/out/artifacts/thuc_hanh_ung_dung_quan_ly__khach_hang_war_exploded/customer/create.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%--
   Created by IntelliJ IDEA.
   User: Dinh Phu
@@ -8,9 +9,44 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>Create New Customer</title>
 </head>
 <body>
-
+<h1>Create new customer</h1>
+<p>
+    <c:if test='${requestScope["message"] != null}'>
+        <span class="message">${requestScope["message"]}</span>
+    </c:if>
+</p>
+<p>
+    <%
+        String mainUrl="http://localhost:8080/thuc_hanh_ung_dung_quan_ly__khach_hang_war_exploded/customers";
+    %>
+    <p><%=mainUrl%></p>
+    <a href="<%=mainUrl%>">Back to customer list</a>
+</p>
+<form method="post">
+    <fieldset>
+        <legend>Customer information</legend>
+        <table>
+            <tr>
+                <td>Name: </td>
+                <td><input type="text" name="name" id="name"></td>
+            </tr>
+            <tr>
+                <td>Email: </td>
+                <td><input type="text" name="email" id="email"></td>
+            </tr>
+            <tr>
+                <td>Address: </td>
+                <td><input type="text" name="address" id="address"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="Create customer"></td>
+            </tr>
+        </table>
+    </fieldset>
+</form>
 </body>
 </html>
--- thuc hanh ung dung quan ly  khach hang/src/com/codegym/controller/CustomerServlet.java
+++ thuc hanh ung dung quan ly  khach hang/src/com/codegym/controller/CustomerServlet.java
@@ -4,6 +4,7 @@ import com.codegym.model.Customer;
 import com.codegym.service.CustomerService;
 import com.codegym.service.CustomerServiceImpl;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "CustomerServlet",urlPatterns = {"/customers"})
+//@WebServlet(name = "CustomerServlet",urlPatterns = {"/customers"})
 public class CustomerServlet extends HttpServlet {
     private CustomerService customerService= new CustomerServiceImpl();
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -23,6 +24,7 @@ public class CustomerServlet extends HttpServlet {
 
         switch(action){
             case "create":
+                createCustomer(request,response);
                 break;
             case "edit":
                 break;
@@ -30,6 +32,24 @@ public class CustomerServlet extends HttpServlet {
                 break;
             default:
                 break;
+        }
+    }
+
+    private void createCustomer(HttpServletRequest request, HttpServletResponse response) {
+        String name= request.getParameter("name");
+        String email= request.getParameter("email");
+        String address= request.getParameter("address");
+        int id=(int)(Math.random()*10000);
+
+        Customer customer = new Customer(id,name,email,address);
+        this.customerService.save(customer);
+        request.setAttribute("message","New customer was created");
+        try {
+            getServletContext().getRequestDispatcher("/customer/create.jsp").forward(request,response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -41,8 +61,10 @@ public class CustomerServlet extends HttpServlet {
 
         switch(action){
             case "create":
+                showCreateForm(request,response);
                 break;
             case "edit":
+                showEditForm(request,response);
                 break;
             case "delete":
                 break;
@@ -52,6 +74,34 @@ public class CustomerServlet extends HttpServlet {
         }
     }
 
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response) {
+        int id= Integer.parseInt(request.getParameter("id"));
+        Customer customer = this.customerService.findById(id);
+        RequestDispatcher dispatcher;
+        if(customer == null){
+            dispatcher = request.getRequestDispatcher("error-404.jsp");
+        } else {
+            this.customerService.remove(id);
+            try {
+                response.sendRedirect("/customers");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void showCreateForm(HttpServletRequest request, HttpServletResponse response) {
+        RequestDispatcher dispatcher=request.getRequestDispatcher("customer/create.jsp");
+        try {
+            dispatcher.forward(request,response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     private void listCustomer(HttpServletRequest request, HttpServletResponse response) {
         List<Customer> customers= this.customerService.findAll();
 
@@ -59,7 +109,6 @@ public class CustomerServlet extends HttpServlet {
 
         try {
             getServletContext().getRequestDispatcher("/customer/list.jsp").forward(request,response);
-
         } catch (ServletException e) {
             e.printStackTrace();
         } catch (IOException e) {
